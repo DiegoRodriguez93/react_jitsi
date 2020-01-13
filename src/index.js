883481cd49c56248bb730a3b9e3783ca6eac9c08
Diego Rodriguez Paiva <diegorodriguez93@hotmail.com>
2020-01-13 07:19:59 -0300

    1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import JitsiMeetComponent from './component/JitsiMeetComponent';
+
+function App(){
+
+    return <JitsiMeetComponent></JitsiMeetComponent>
+
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'));
